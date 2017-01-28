629035635988e17d6f0a0a68efadf53813ac12c1
Richard Nienaber <rjnienaber@gmail.com>
2017-01-28 16:22:23 +0000

    add bootstrap

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, './app/index.html'),
     protocol: 'file:',
     slashes: true
   }))
