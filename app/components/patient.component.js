44c669ccc66a5206170979a833b580a93c949aaa
vvcb <iksu.2002@gmail.com>
2017-01-29 11:32:15 +0000

    Halfway through UI changes

--- app/components/patient.component.js
+++ app/components/patient.component.js
@@ -1,119 +1,124 @@
 Vue.component('patient', {
-  props: ['item'],
-  template: `<div>
+props: ['item'],
+template: `<div>
   <div class="row">
-  
-    <div class="col-xs-3 col-md-1">
-      <label for="exampleInputEmail1">Ward</label>
-      <input type="email" v-model="item.wAR" class="form-control" id="exampleInputEmail1" placeholder="Email">
-    </div>
-    
-    <div class="col-xs-3 col-md-1">
-      <label for="exampleInputEmail1">Bed</label>
-      <input type="email" v-model="item.name" class="form-control" id="exampleInputEmail1" placeholder="Email">
-    </div>	
-  
-    <div class="col-xs-6 col-md-4">
-      <label for="exampleInputEmail1">Problem</label>
-      <input type="email" v-model="item.problem" class="form-control" id="exampleInputEmail1" placeholder="Email">  
-    </div>
-  
-    <div class="col-xs-6 col-md-3">	  
-        <label for="exampleInputEmail1">Treatment</label>
-      <input type="email" v-model="item.treatment" class="form-control" id="exampleInputEmail1" placeholder="Email">  
-    </div>
-  
-    <div class="col-xs-3 col-md-2">
-      <label for="exampleInputEmail1">Op date</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">  
-    </div>
-  
-  </div>
-  
-  <div class="row">
-  
-    <div class="col-xs-6 col-md-2">
-      <label for="exampleInputEmail1">Name</label>
-      <input type="email" v-model="item.name" class="form-control" id="exampleInputEmail1" placeholder="Email">
-      
+    <div class="col-md-2">
       <div class="row">
-            <div class="col-xs-12 col-sm-12">
-            <label for="exampleInputEmail1">DOB</label>
-              <input type="email" v-model="item.dob" class="form-control" id="exampleInputEmail1" placeholder="Email">
-            </div>
-       </div>
-       
-       <div class="row">
-            <div class="col-xs-12 col-sm-12">
-            <label for="exampleInputEmail1">HOSP #</label>
-              <input type="email" v-model="item.uid" class="form-control" id="exampleInputEmail1" placeholder="Email">
-            </div>
-       </div> 
-       
-       <div class="row">
-            <div class="col-xs-6 col-sm-6">
-            <label for="exampleInputEmail1">TEAM</label>
-              <input type="email" v-model="item.team" class="form-control" id="exampleInputEmail1" placeholder="Email">
-            </div>
-            
-            <div class="col-xs-6 col-sm-6">
-            <label for="exampleInputEmail1">COMS</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-            </div>
-       </div> 	      
+        <div class="col-md-6">
+          <input type="text" v-model="item.wAR" class="form-control" id="patman-" placeholder="Ward">
+        </div>
+        <div class="col-md-6">
+          <input type="text" v-model="item.bed" class="form-control" id="patman-" placeholder="Bed">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <input type="text" v-model="item.name" class="form-control" id="patman-" placeholder="Name">
+        </div>
+      </div>
+    
+    <div class="row">
+      <div class="col-md-12">
+        <input type="text" v-model="item.dob" class="form-control" id="patman-" placeholder="DOB">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+      <input type="text" v-model="item.uid" class="form-control" id="patman-" placeholder="Patient ID">
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-md-6">
+        <input type="text" v-model="item.team" class="form-control" id="patman-" placeholder="Team">
+      </div>
+        <div class="col-md-6">
+        <input type="text" v-model="item.consultant" class="form-control" id="patman-" placeholder="Cons">
+      </div>
+    </div>
+    </div>
+    <div class="col-md-4" id="Diagnosis-Present-Past">
       
-    </div>	
-  
+    </div>
+
+    <div class="col-md-4" id="Treatment-TreatDate-Tests">
+      
+    </div>
+
+  <div class="col-md-4" id="Jobs-AdverseEvents">
+      
+    </div>
     <div class="col-xs-6 col-md-4">
-      <label for="exampleInputEmail1">Details</label>
-      <input type="email" v-model="item.details" class="form-control" id="exampleInputEmail1" placeholder="Email">  
+      <label for="patman-">Problem</label>
+      <input type="text" v-model="item.problem" class="form-control" id="patman-" placeholder="Email">
     </div>
-  
-    <div class="col-xs-6 col-md-3">	  
-        <label for="exampleInputEmail1">Test results</label>
-      <input type="email" v-model="item.tests" class="form-control" id="exampleInputEmail1" placeholder="Email">  
-    </div>
-  
+    
     <div class="col-xs-6 col-md-3">
-      <label for="exampleInputEmail1">Jobs</label>
-      <input type="email" v-model="item.jobs" class="form-control" id="exampleInputEmail1" placeholder="Email">  
+      <label for="patman-">Treatment</label>
+      <input type="text" v-model="item.treatment" class="form-control" id="patman-" placeholder="Email">
     </div>
+    
+    <div class="col-xs-3 col-md-2">
+      <label for="patman-">Op date</label>
+      <input type="text" class="form-control" id="patman-" placeholder="Email">
+    </div>
+    
+  </div>
   
+  <div class="row">
+    
+   
+    
+    <div class="col-xs-6 col-md-4">
+      <label for="patman-">Details</label>
+      <input type="text" v-model="item.details" class="form-control" id="patman-" placeholder="Email">
+    </div>
+    
+    <div class="col-xs-6 col-md-3">
+      <label for="patman-">Test results</label>
+      <input type="text" v-model="item.tests" class="form-control" id="patman-" placeholder="Email">
+    </div>
+    
+    <div class="col-xs-6 col-md-3">
+      <label for="patman-">Jobs</label>
+      <input type="text" v-model="item.jobs" class="form-control" id="patman-" placeholder="Email">
+    </div>
+    
   </div>
   
   
   <div class="row">
-  
+    
     <div class="col-xs-3 col-md-2">
-      <label for="exampleInputEmail1">Admitted</label>
-      <input type="email" v-model="item.adm_date" class="form-control" id="exampleInputEmail1" placeholder="Email">
-    </div>	
+      <label for="patman-">Admitted</label>
+      <input type="text" v-model="item.adm_date" class="form-control" id="patman-" placeholder="Email">
+    </div>
     
     <div class="col-xs-6 col-md-4">
-      <label for="exampleInputEmail1">PMH</label>
-      <input type="email" v-model="item.past_medical_history" class="form-control" id="exampleInputEmail1" placeholder="Email">
-    </div>	
+      <label for="patman-">PMH</label>
+      <input type="text" v-model="item.past_medical_history" class="form-control" id="patman-" placeholder="Email">
+    </div>
     
     <div class="col-xs-6 col-md-3">
-      <label for="exampleInputEmail1">Adverse Events</label>
-      <input type="email" v-model="item.adverse_events" class="form-control" id="exampleInputEmail1" placeholder="Email">
-    </div>	
+      <label for="patman-">Adverse Events</label>
+      <input type="text" v-model="item.adverse_events" class="form-control" id="patman-" placeholder="Email">
+    </div>
     
-    <div class="col-xs-6 col-md-2">	
-    <label>
+    <div class="col-xs-6 col-md-2">
+      <label>
         <input type="checkbox"> Check me out
       </label>
-    </div>	
-  
+    </div>
+    
   </div>
   <div>
     <button v-on:click="updateClick">Update</button>
   </div>
 </div>
 `,
-  methods: {
-    updateClick: function () {
-      this.$emit('update-click', this.item);
-    }
-  },
+methods: {
+updateClick: function () {
+this.$emit('update-click', this.item);
+}
+},
 });
