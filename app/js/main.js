e741471245d57fd6c037404c4db9435f88ce8333
Richard Nienaber <rjnienaber@gmail.com>
2017-01-29 06:35:51 +0000

    working Vue component

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 function remoteRequire(module) {
   return require('electron').remote.require(module);
 }
+
+$(() => {
+  // example communication between main and render process
+  const { getFile } = remoteRequire('./app/getFile');
+  $('#primary').click(() => {
+    console.log(getFile);
+    getFile('yarn.lock').then((c) => console.log(c));
+  });
+  var app7 = new Vue({
+      el: '#app-7',
+      data: {
+          groceryList: [
+              { text: 'Vegetables' },
+              { text: 'Cheese' },
+              { text: 'Whatever else humans are supposed to eat' }
+          ]
+      }
+   });
+});
