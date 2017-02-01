0c2a9d1799d96ec392d2688f8c63e66f538ba8ff
vvcb <iksu.2002@gmail.com>
2017-02-01 12:07:57 +0000

    Search section componentized

--- app/components/search.component.js
+++ app/components/search.component.js
@@ -1,7 +1,55 @@
-Vue.component('search', {  // eslint-disable-line no-undef
-	
-	template: `<div>
-		<p> If this works, this is magic</p>
-	</div>`,
+Vue.component('search-patient', {  // eslint-disable-line no-undef
 
-	});
+template: `
+<div class="row patient" id="SearchPanel">
+	<!--
+		<div class="col-md-2">
+								<h3 @click="toggleFilters()">
+								<span class="glyphicon glyphicon-search"></span>
+					Search
+		</div>
+	-->
+	<div class="col-md-3">
+		<div class="input-group input-group-sm">
+			<span class="input-group-addon" id="sizing-addon3">Search Patient: Id </span>
+			<input @keyup="search" type="text" v-model="searchCriteria.uid" class="form-control" placeholder="Hospital Number" aria-describedby="sizing-addon3">
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="input-group input-group-sm">
+			<span class="input-group-addon" id="sizing-addon3">Name: </span>
+			<input @keyup="search" type="text" v-model="searchCriteria.name" class="form-control" placeholder="Patient Name" aria-describedby="sizing-addon3">
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="input-group input-group-sm">
+			<span class="input-group-addon" id="sizing-addon3">Ward: </span>
+			<select style="height: 30px;" @change="search" name="wards" v-model="searchCriteria.filters.ward">
+				<option v-for="ward in searchCriteria.availableWards" v-bind:value="ward.name" aria-describedby="sizing-addon3">
+					{{ ward.name }}
+				</option>
+			</select>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="input-group input-group-sm">
+			<span class="input-group-addon" id="sizing-addon3">Team: </span>
+			<select style="height: 30px;" @change="search" name="team" v-model="searchCriteria.filters.team">
+				<option v-for="team in searchCriteria.availableTeams" v-bind:value="team.code" aria-describedby="sizing-addon3">
+					{{ team.code }}
+				</option>
+			</select>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="input-group input-group-sm">
+			<span class="input-group-addon" id="sizing-addon3">Cons: </span>
+			<select style="height: 30px;" @change="search" name="team" v-model="searchCriteria.filters.consultant">
+				<option v-for="consultant in searchCriteria.availableConsultants" v-bind:value="consultant.initials" aria-describedby="sizing-addon3">
+					{{ consultant.initials }}
+				</option>
+			</select>
+		</div>
+	</div>
+</div>`
+})
