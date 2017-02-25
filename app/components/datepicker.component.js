2da89eac65e42d49dfe8643a2d0ccc5b26a21bda
Richard Nienaber <rjnienaber@gmail.com>
2017-02-25 16:48:39 +0000

    Fix blur event erasing input

--- app/components/datepicker.component.js
+++ app/components/datepicker.component.js
@@ -2,10 +2,16 @@
 
 Vue.component('datepicker', {   // eslint-disable-line no-undef
   props: ['instance', 'name'],
-  template: `<input type="text" v-text="moment(instance[name]).format('DD-MM-YYYY')" readonly="readonly" class="form-control datepicker" aria-describedby="sizing-addon3">
+  template: `<input type="text" readonly="readonly" class="form-control datepicker" aria-describedby="sizing-addon3">
 `,
   mounted() {
     const component = this;
+    const currentDate = () =>  {
+      const value = component.instance[component.name];
+      return value ? moment(value).format('DD-MM-YYYY') : '';
+    };
+
+
     $(this.$el).datepicker({
       format: 'dd-mm-yyyy',
       daysOfWeekHighlighted: [0,6],
@@ -14,9 +20,10 @@ Vue.component('datepicker', {   // eslint-disable-line no-undef
       weekStart: 1,
     }).on('changeDate', function(e) {
       component.instance[component.name] = moment($(e.target).val(), 'DD-MM-YYYY').toDate();
+    }).val(currentDate());
+
+    $(this.$el).blur(function () {
+      $(this).val(currentDate());
     });
-    const value = component.instance[component.name];
-    if (value)
-      $(component.$el).val(moment(value).format('DD-MM-YYYY'));
   }
 });
